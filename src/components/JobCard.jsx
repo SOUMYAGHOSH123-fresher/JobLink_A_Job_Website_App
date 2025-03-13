afbb5fc88e5b96e5bc1ca5b28d12f5dbaf08e874
SOURAVGHOSH123 <sg608251@gmail.com>
2025-03-14 00:08:41 +0530

    jobCard,Application,assets,Loading,Navbar,Applyjob,index

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
-function JobCard({job}) {
+function JobCard({ job }) {
+
+  const navigate = useNavigate()
+
   return (
-      <div className='border p-6 shadow rounded'>
-          <div className='flex justify-between items-center'>
-            <img className='h-10' src={assets.company_icon } alt="" />
-          </div>
-          <h4 className='font-medium text-xl mt-3'>{job.title}</h4>
-          <div className='flex items-center gap-3 mt-2 text-xs'>
-              <span className='bg-blue-50 border border-blue-200 px-4 py-1.5 text-md font-medium'>{ job.location}</span>
-              <span className='bg-red-50 border border-red-200 px-4 py-1.5 text-md font-medium'>{ job.level }</span>
-          </div>
-          <p className='text-gray-500 text-sm mt-2 text-justify' dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}></p>
-          <div className='flex gap-3 mt-3 px-2 text-sm'>
-              <button className=' bg-blue-500 text-gray-50 px-3 py-2 rounded shadow'>Apply Now</button>
-              <button className=' text-gray-500 border border-gray-500 px-4 py-2'>Learn More 🎓</button>
-          </div>
+    <div className='border p-6 shadow rounded'>
+      <div className='flex justify-between items-center'>
+        <img className='h-10' src={assets.company_icon} alt="" />
+      </div>
+      <h4 className='font-medium text-xl mt-3'>{job.title}</h4>
+      <div className='flex items-center gap-3 mt-2 text-xs'>
+        <span className='bg-blue-50 border border-blue-200 px-4 py-1.5 text-md font-medium'>{job.location}</span>
+        <span className='bg-red-50 border border-red-200 px-4 py-1.5 text-md font-medium'>{job.level}</span>
+      </div>
+      <p className='text-gray-500 text-sm mt-2 text-justify' dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}></p>
+      <div className='flex gap-3 mt-3 px-2 text-sm'>
+        <button onClick={() => { navigate(`/apply-job/${job._id}`); scrollTo(0, 0) }} className=' bg-blue-500 text-gray-50 px-3 py-2 rounded shadow'>Apply Now</button>
+        <button onClick={() => { navigate(`/apply-job/${job._id}`); scrollTo(0, 0) }} className=' text-gray-500 border border-gray-500 px-4 py-2'>Learn More 🎓</button>
+      </div>
     </div>
   )
 }
