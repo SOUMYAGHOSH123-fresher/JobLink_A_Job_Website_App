caa1972fb65ca398c48124da288b1c6098065fbd
SOURAVGHOSH123 <sg608251@gmail.com>
2025-03-14 00:24:44 +0530

    jobCard,Application,assets,Loading,Navbar,Applyjob,index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { assets } from '../assets/assets';
-import {useClerk, UserButton, useUser} from '@clerk/clerk-react';
-import { Link } from 'react-router-dom';
-
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
+  const navigate = useNavigate();
+
   return (
     <div className="shadow py-4">
       <div className='container px-4 5x1:px-20 mx-auto flex justify-between items-center'>
         <h1 className='text-3x1 px-auto'>JobLink</h1>
+        <Link to='/'><button className='text-gray-600'>Home</button></Link>
         {
           user
             ?
             <div className='flex items-center gap-3'>
-            <Link to={'/applications'}> Applied Jobs</Link>
-            <p></p>
-            <p className='max-sm:hidden'>Hi, {user.firstName + " " + user.lastName}</p>
-            <UserButton />
-          </div> 
-          : <div className='flex gap-4 max-sm:text-xs'>
+              <Link to={'/applications'}> Applied Jobs</Link>
+              <p></p>
+              <p className='max-sm:hidden'>Hi, {user.firstName + " " + user.lastName}</p>
+              <UserButton />
+            </div>
+            : <div className='flex gap-4 max-sm:text-xs'>
               <button className='text-gray-600'>Recruiter Login</button>
-              <button onClick={ e=> openSignIn() } className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
+              <button onClick={e => openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
             </div>
         }
-        
+
       </div>
     </div>
   )
